01bb1204562ebc448e803425f9c46a023efe9541
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-11-28 17:43:08 +0700

    28/11/2022

--- index.js
+++ index.js
@@ -1,158 +1,31 @@
- // CallBack?
-
- // Là Hàm (function) được truyền qua đối số
- // Khi gọi hàm khác
-
- // 1. là hàm
- // 2. Truyền qua đối số của một function khác gọi là callBack
-
-
-function myFunction(param)   {
-
-    if (typeof param === 'function') {
-        param('Hoc lap trinh')
+var inputElement = document.querySelector('input[type="text"]')
+    inputElement.oninput = function(e) {
+        console.log(e.target.value)
     }
-}
 
-
-function myCallback(value) { 
-    console.log('Value: ', value )
-}
-
-
-
-myFunction(myCallback) 
-
-
-// CallBack phần 2 -------------------------------------------
-// 1. Là hàm
-// 2. Truyền qua đối số
-// 3. Được gọi lại (trong hàm nhận đối số)
-
-
-var courses = [
-    'Javascript',
-    'PHP',
-    'Ruby'
-]
-
-
-
-Array.prototype.map2 = function(callback) {
-    var output = []
-    var arrayLength = this.length;
-    
-     for(var i = 0; i < arrayLength; ++i) {
-       var result = callback(this[i], i);
-        output.push(result)
-     } 
-
-     return output;
-}
-
-
-
-var htmls = courses.map2(function(course) {
-    return `<h2>${course}</h2>` 
-});
-
-console.log(htmls.join(''))
-
-
-
-
-
-// var htmls = courses.map(function(course) {  
-//     return `<h2>${course}</h2>`
-// });
-
-// console.log(htmls.join(''));
-
-
-
-
-
-
-var courses1 = [
-    'Ngocrongonline',
-    'PHP',
-    'dreamelengausoccer'
-]
-
-Array.prototype.map3 = function(callback) {
-    var output = []
-    var arrayLength = this.length
-
-    for (var i = 0; i < arrayLength; ++i) {
-        var result = callback(this[i], i);
-        output.push(result)
-    } 
-    return output
-}
-
- 
-var htmls =  courses1.map3(function(course) {      
-    return `<h2>${course}</h2>` 
-})
-
-console.log(htmls)
-
-
-// courses1.map(function (course) {
-//     return `<h2>${course}</h2>`
-// })
-
-// console.log(htmls.join(''))
-
-
-var course2 = [
-    'Khoa hoc reacjs',
-    'Khoa hoc html,css',
-    'Khoa hoc Javarscript'
-]
-
-
-Array.prototype.forEach = function(cb) {
-    var Output = []
-    var arrayLength = this.length
-
-    for (var i = 0; i < arrayLength; i++) {
-        var result = cb(this[i],i)
-        Output.push(result)
+var checkElement = document.querySelector('input[type="checkbox"]')
+    checkElement.onchange = function(e) {
+        console.log(e.target.checked)
     }
-        return Output
 
-}
+let selectElement = document.querySelector('select')
+    selectElement.oninput = function(e) {
+        console.log(e.target.value)
+    }
 
+// keyborud
+    document.onkeydown = function(e) {
+        console.log(e.which)
 
-var javascripts = course2.forEach(function(course) {
-    return `<h2>${course2}</h2>` 
-})
+        switch (e.which) {
+            case 27 :
+            console.log('I LOVE YOU SO MUCH')
+            break
 
-console.log(javascripts)
+            case 65:
+            console.log('I LOVE YOU TOO')
+            break
+        
+        }
 
-
-
-// Empty element of array? ----------------------------------------------
-
-// var length1 = [
-//     'Javarscript',
-//     'PHP'
-// ]
-
-// for (var index in length1) {
-//     console.log(length1[index])
-// }
-
-
-// var length2 = new Array(10)
-// length2.push('javascript', 'PHP')
-
-// console.log(length2);
-// for (var index in length2) {
-//     console.log(length2[index]) 
-// }
-
-
-var myString = 'Javascript bazzic'
-console.log(myString. charAt(3))
+    }
